8c9a372cbd91e565190aa3c512295794598c3589
Hussen Fakhrulddin <hf556482@gmail.com>
2016-05-20 22:52:59 -0700

    init

---
+++ Tribonacci/Tribonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tribonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] signature;
+            signature = Convert.ToCharArray(Console.ReadLine());
+
+            Xbonacci.Tribonacci(signature, n);
+        }
+    }
+}
+public class Xbonacci
+{
+    public static double[] Tribonacci(double[] signature, int n)
+    {
+
+        for (int i = 0; i < (n - 3); i++)
+        {
+
+            signature[i + 3] = signature[i] + signature[i + 1] + signature[i + 2];
+
+        }
+        return signature;
+    }
+}
